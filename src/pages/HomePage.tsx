1a784271bd8a624cafffe8edf685fd34847c2806
djiang9001 <danieljng2005@gmail.com>
2025-09-27 18:52:06 -0400

    put resume on homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,6 +7,9 @@ import { Panel } from '@app/components/Panel';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ResumeIcon from '@mui/icons-material/Article';
+
+import Resume from '@app/assets/resume.pdf'
 
 export const HomePage: FC = () => {
 
@@ -19,6 +22,7 @@ export const HomePage: FC = () => {
         <Link sx={{ display: "flex", justifyContent: "center" }} color="inherit" href="https://github.com/djiang9001" target="_blank" rel="noreferrer"><GitHubIcon/></Link>
         <Link sx={{ display: "flex", justifyContent: "center" }} color="inherit" href="https://www.linkedin.com/in/djiang9001" target="_blank" rel="noreferrer"><LinkedInIcon/></Link>
         <Link sx={{ display: "flex", justifyContent: "center" }} color="inherit" href="mailto:danieljng2005@gmail.com"><EmailIcon/></Link>
+        <Link sx={{ display: "flex", justifyContent: "center" }} color="inherit" href={Resume} target="_blank" rel="noreferrer"><ResumeIcon/></Link>
       </Box>
     </Panel>
   )
